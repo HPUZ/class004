7eb0244acd42afb8b79359a2518e8c82a28df386
John <hpusec@gmail.com>
2017-10-10 14:55:33 +0800

    Add 004

---
+++ age.rb
@@ -0,0 +1,6 @@
+def user(age,n=0)
+  4.times do 
+    n+=10
+    puts "In #{ n } years you will be: #{ age + n }"
+  end
+end
---
+++ name.rb
@@ -0,0 +1,23 @@
+# 1
+def user(name)
+  "hi,#{name}!"
+
+  10.times do 
+    p name
+  end
+end
+
+# 4
+def first_name(name)
+  @first_name = name
+end
+
+def last_name(name)
+  @last_name = name
+  puts "#{@first_name} #{@last_name}"
+end
+
+# 9 
+def cheng(a,b)
+  
+end
